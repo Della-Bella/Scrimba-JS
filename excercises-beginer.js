942577538ef95cb0cc763946c50a168ea017e06e
Della-Bella <gisdellabella@uol.com.br>
2024-11-01 21:58:44 +0000

    button count function done

---
+++ excercises-beginer.js
@@ -0,0 +1,34 @@
+// document.getelementById("count")innerText=5
+
+// 1.creatte variable , MyAge, and set its vlaleus to your age
+
+//2 . log thevariable to the console log
+
+   // let count = 0
+   // let myAge=49
+    //console.log ( "My age is " + myAge )
+
+
+//let myAge = 35
+//let humanDogRatio = 7
+
+//let myDogAge = myAge * humanDogRatio
+
+//console.log(myDogAge)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+
+//let bonusPoints = 50;
+//console.log(bonusPoints);
+
+//bonusPoints = bonusPoints + 100;
+//console.log(bonusPoints);
+
+//bonusPoints = bonusPoints - 25;
+//console.log(bonusPoints);
+//bonusPoints = bonusPoints + 70;
+
+//console.log(bonusPoints);
