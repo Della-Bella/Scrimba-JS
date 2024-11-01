086a01bba75331fe9b76b188e8b11894ded623b9
Della-Bella <gisdellabella@uol.com.br>
2024-11-01 20:36:04 +0000

    counting-people-exercises

---
+++ main.js
@@ -0,0 +1,24 @@
+// document.getelementById("count")innerText=5
+
+// 1.creatte variable , MyAge, and set its vlaleus to your age
+
+//2 . log thevariable to the console log
+
+   // let count = 0
+   // let myAge=49
+    //console.log ( "My age is " + myAge )
+
+
+//let myAge = 35
+//let humanDogRatio = 7
+
+//let myDogAge = myAge * humanDogRatio
+
+//console.log(myDogAge)
+
+
+
+
+
+
+
