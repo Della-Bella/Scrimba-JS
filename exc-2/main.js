714e56c53d4824e180aef7210544824ed5293ddf
Della-Bella <gisdellabella@uol.com.br>
2024-11-05 20:16:24 +0000

    exercise variable name

---
+++ exc-2/main.js
@@ -0,0 +1,10 @@
+// Create two variables, firstName and lastName
+
+// Concatenate the two variables into a third variable called fullName
+
+// Log fullName to the console
+
+var firstName = "Gislaine";
+var lastName = "Della Bella";
+var fullName = firstName + " " + lastName;  // Adds a space between first and last names
+console.log(fullName);
